fdf723badd0d63259706164a423e0f59378f64a2
elmogdadhassan1991@gmail.com <elmogdadhassan1991@gmail.com>
2025-01-14 09:18:36 +0300

    install packages and make main folders

---
+++ E-commerce-filter/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import "./App.css"
+import Nav from "./Navigation/Nav"
+import Products from "./Porducts/Products"
+import Sidebar from "./sidebar/Sidebar"
+
+const App = () => {
+  return (
+  <>
+  <Nav />
+  <Products />
+  <Sidebar />
+  </>
+  )
+}
+
+export default App
